3c0a2dcdd878254abb13254abdf5a34461509e42
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2022-01-18 19:55:17 +0330

    Updates

---
+++ precache-manifest.48a7d5f28db22b0cd4fd39e168f65e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d972d6dcc2fc4b2e16e5",
+    "url": "/Mohsenpoureiny/resume-react/static/css/main.7c7e05e2.chunk.css"
+  },
+  {
+    "revision": "d972d6dcc2fc4b2e16e5",
+    "url": "/Mohsenpoureiny/resume-react/static/js/main.d972d6dc.chunk.js"
+  },
+  {
+    "revision": "3a39d6df14ccba5ab9a2",
+    "url": "/Mohsenpoureiny/resume-react/static/js/1.3a39d6df.chunk.js"
+  },
+  {
+    "revision": "1b4d5c26a78ab3dc85ad",
+    "url": "/Mohsenpoureiny/resume-react/static/js/runtime~main.1b4d5c26.js"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/Mohsenpoureiny/resume-react/static/media/bg.efdba046.jpg"
+  },
+  {
+    "revision": "c4af7cc2bb561f4048ad0eb3326799eb",
+    "url": "/Mohsenpoureiny/resume-react/index.html"
+  }
+];
